77789382fd4d70e888371dd27bc91cef8983a4cf
reyluar18 <33047940+reyluar18@users.noreply.github.com>
2018-10-25 07:29:28 +0800

    Create Updater.js

---
+++ Updater.js
@@ -0,0 +1,12 @@
+[
+  {
+    "AppName": "Panda VPN",
+    "AppVersion": "7.0",
+    "Author": "Rey Luar Jr."
+  },
+  {
+    "AppName": "Panda VPN Pro",
+    "AppVersion": "8.0",
+    "Author": "Rey Luar Jr."
+  }
+]
